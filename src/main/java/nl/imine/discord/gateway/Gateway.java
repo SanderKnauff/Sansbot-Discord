72c6618980e8ca5ff0f7fb84ebdb2de1ccface24
Sansko1337 <sanderknauff@hotmail.com>
2017-11-01 18:07:01 +0100

    Optimize imports and reformat code in Gateway.java

--- src/main/java/nl/imine/discord/gateway/Gateway.java
+++ src/main/java/nl/imine/discord/gateway/Gateway.java
@@ -1,9 +1,7 @@
 package nl.imine.discord.gateway;
 
-import java.net.URI;
-import java.util.HashMap;
-
 import nl.imine.discord.event.EventDispatcher;
+import nl.imine.discord.util.Rest;
 import nl.imine.vaccine.annotation.Component;
 import nl.imine.vaccine.annotation.Property;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
@@ -13,50 +11,50 @@ import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import nl.imine.discord.util.Rest;
-
 import javax.annotation.PostConstruct;
+import java.net.URI;
+import java.util.HashMap;
 
 @Component
 public class Gateway {
 
-	private static final Logger logger = LoggerFactory.getLogger(Gateway.class);
+    private static final Logger logger = LoggerFactory.getLogger(Gateway.class);
 
-	private final EventDispatcher eventDispatcher;
-	private final Rest rest;
-	private final String botToken;
+    private final EventDispatcher eventDispatcher;
+    private final Rest rest;
+    private final String botToken;
 
-	public Gateway(@Property("discord.client.token") String botToken, Rest rest, EventDispatcher eventDispatcher) {
-		this.rest = rest;
-		this.botToken = botToken;
-		this.eventDispatcher = eventDispatcher;
-	}
+    public Gateway(@Property("discord.client.token") String botToken, Rest rest, EventDispatcher eventDispatcher) {
+        this.rest = rest;
+        this.botToken = botToken;
+        this.eventDispatcher = eventDispatcher;
+    }
 
-	@PostConstruct
-	public void postConstruct(){
-		try {
-			this.openWebSocket();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @PostConstruct
+    public void postConstruct() {
+        try {
+            this.openWebSocket();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void openWebSocket() throws Exception {
-		JSONObject ret = rest.getSync("https://discordapp.com/api/gateway", new HashMap<>());
-		String url = (String) ret.get("url") + "/?v=6&encoding=json";
-		logger.info("Received Gateway url: {}", url);
+    public void openWebSocket() throws Exception {
+        JSONObject ret = rest.getSync("https://discordapp.com/api/gateway", new HashMap<>());
+        String url = (String) ret.get("url") + "/?v=6&encoding=json";
+        logger.info("Received Gateway url: {}", url);
 
-		SslContextFactory ssl = new SslContextFactory();
-		WebSocketClient webSocket = new WebSocketClient(ssl);
+        SslContextFactory ssl = new SslContextFactory();
+        WebSocketClient webSocket = new WebSocketClient(ssl);
 
-		webSocket.getPolicy().setMaxBinaryMessageSize(Integer.MAX_VALUE);
-		webSocket.getPolicy().setMaxTextMessageSize(Integer.MAX_VALUE);
-		ClientUpgradeRequest request = new ClientUpgradeRequest();
-		request.setHeader("Authorization", "bot " + botToken);
-		webSocket.start();
-		GatewaySocket gatewaySocket = new GatewaySocket(botToken, eventDispatcher);
-		webSocket.connect(gatewaySocket, new URI(url), request);
+        webSocket.getPolicy().setMaxBinaryMessageSize(Integer.MAX_VALUE);
+        webSocket.getPolicy().setMaxTextMessageSize(Integer.MAX_VALUE);
+        ClientUpgradeRequest request = new ClientUpgradeRequest();
+        request.setHeader("Authorization", "bot " + botToken);
+        webSocket.start();
+        GatewaySocket gatewaySocket = new GatewaySocket(botToken, eventDispatcher);
+        webSocket.connect(gatewaySocket, new URI(url), request);
 
-		logger.info("WebSocket is connected: {}", gatewaySocket.isConnected());
-	}
+        logger.info("WebSocket is connected: {}", gatewaySocket.isConnected());
+    }
 }
